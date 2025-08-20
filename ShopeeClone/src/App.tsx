1359e8851b0ec797efe6d45b197a2529305b2f5e
Tuandev18 <phamtuan180704@gmail.com>
2025-08-20 20:28:39 +0700

    Enable sourcemap for css and fix import path error

--- ShopeeClone/src/App.tsx
+++ ShopeeClone/src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  return <div className='bg-red-500 text-white p-10'>Tailwind test</div>
+  return <div className='bg-red-500 text-white p-10 '>Tailwind test</div>
 }
 
 export default App
