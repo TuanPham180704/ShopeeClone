354dd194b7dfc625dba3653f50eba9f5662639ec
Tuandev18 <phamtuan180704@gmail.com>
2025-08-28 21:32:33 +0700

    Code UI Product Component

--- ShopeeClone/src/pages/ProductList/Product/Product.tsx
+++ ShopeeClone/src/pages/ProductList/Product/Product.tsx
@@ -1,7 +1,63 @@
-
+import { Link } from 'react-router-dom'
 
 export default function Product() {
   return (
-    <div>Product</div>
+    <Link to='/'>
+      <div className='bg-white shadow rounded-sm hover:translate-y-[-0.04rem] hover:shadow-md duration-100 transition-transform overflow-hidden'>
+        <div className='w-full pt-[100%] relative'>
+          <img
+            src='https://cf.shopee.vn/file/ea0f159f3f4c713abcf56b5ba73840b9'
+            alt=''
+            className='absolute top-0 left-0 bg-white w-full h-full object-cover'
+          />
+        </div>
+        <div className='p-2 overflow-hidden'>
+          <div className='min-h-[2rem] line-clamp-2 text-xs '>
+            [HÀNG HIỆU] Thắt Lưng Da Nam Khóa Tự Động Cao Cấp Dây Nịt Nam Mặt Xoay Chính Hãng , Phong Cách Hàn Quốc -
+            v77men
+          </div>
+          <div className='flex items-center mt-3'>
+            <div className='line-through max-w-[50%] text-gray-500 truncate'>
+              <span className='text-xs'>₫</span>
+              <span>5.000</span>
+            </div>
+            <div className='text-orange truncate ml-1'>
+              <span className='text-xs'>₫</span>
+              <span>2.000</span>
+            </div>
+          </div>
+          <div className='mt-3 flex items-center justify-end'>
+            <div className='flex items-center relative w-3 h-3'>
+              {/* Sao đầy */}
+              <svg viewBox='0 0 15 15' className='absolute top-0 left-0 w-3 h-3 fill-yellow-300'>
+                <polygon
+                  points='7.5 .8 9.7 5.4 14.5 5.9 10.7 9.1 11.8 14.2 7.5 11.6 3.2 14.2 4.3 9.1 1.5 5.9 5.3 5.4'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeMiterlimit={10}
+                />
+              </svg>
+
+              {/* Lớp mask 50% (che từ phải sang trái) */}
+              <div className='absolute top-0 right-0 h-full bg-white' style={{ width: '50%' }}></div>
+
+              {/* Sao rỗng */}
+              <svg viewBox='0 0 15 15' className='w-3 h-3 fill-current text-gray-300'>
+                <polygon
+                  points='7.5 .8 9.7 5.4 14.5 5.9 10.7 9.1 11.8 14.2 7.5 11.6 3.2 14.2 4.3 9.1 1.5 5.9 5.3 5.4'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeMiterlimit={10}
+                />
+              </svg>
+            </div>
+            <div className='ml-2 text-sm'>
+              <span>5.66k</span>
+              <span className='ml-1'>Đã bán</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
   )
 }
