bb8ae11486839ec630aa24113890e39e3df435fa
Tuandev18 <phamtuan180704@gmail.com>
2025-08-31 15:31:30 +0700

    Handle CreateAt

--- ShopeeClone/src/pages/ProductList/SortProductList/SortProductList.tsx
+++ ShopeeClone/src/pages/ProductList/SortProductList/SortProductList.tsx
@@ -1,20 +1,51 @@
-export default function SortProductList() {
+import type { QueryConfig } from 'src/pages/ProductList/ProductList'
+import { sortBy } from 'src/constants/product'
+import classNames from 'classnames'
+import type { ProductListConfig } from 'src/types/product.type'
+
+interface Props {
+  queryConfig: QueryConfig
+  pageSize: number
+}
+
+export default function SortProductList({ queryConfig, pageSize }: Props) {
+  const { sort_by = sortBy.createdAt } = queryConfig
+
+  const isActiveSortBy = (sortByValue: Exclude<ProductListConfig['sort_by'], undefined>) => {
+    return sort_by === sortByValue
+  }
+
   return (
-    <div className='bg-gray-300/40 py-4 px-3'>
+    <div className='bg-gray-300/40 px-3 py-4'>
       <div className='flex flex-wrap items-center justify-between gap-2'>
-        <div className='flex items-center flex-wrap gap-2'>
+        <div className='flex flex-wrap items-center gap-2'>
           <div>Sắp xếp theo</div>
-          <button className='h-8 px-4 capitalize bg-orange text-white text-sm hover:bg-orange/80 text-center'>
+          <button
+            className={classNames('h-8 bg-orange px-4 text-center text-sm capitalize ', {
+              'bg-orange text-white hover:bg-orange/80': isActiveSortBy(sortBy.view),
+              'bg-white text-black hover:bg-slate-100': !isActiveSortBy(sortBy.view)
+            })}
+          >
             Phổ biến
           </button>
-          <button className='h-8 px-4 capitalize bg-white text-black text-sm hover:bg-slate-100 text-center'>
+          <button
+            className={classNames('h-8 bg-orange px-4 text-center text-sm capitalize ', {
+              'bg-orange text-white hover:bg-orange/80': isActiveSortBy(sortBy.createdAt),
+              'bg-white text-black hover:bg-slate-100': !isActiveSortBy(sortBy.createdAt)
+            })}
+          >
             Mới nhất
           </button>
-          <button className='h-8 px-4 capitalize bg-white text-black text-sm hover:bg-slate-100 text-center'>
+          <button
+            className={classNames('h-8 bg-orange px-4 text-center text-sm capitalize ', {
+              'bg-orange text-white hover:bg-orange/80': isActiveSortBy(sortBy.sold),
+              'bg-white text-black hover:bg-slate-100': !isActiveSortBy(sortBy.sold)
+            })}
+          >
             Bán chạy
           </button>
           <select
-            className='outline-none h-8 px-4 capitalize bg-white text-black text-sm hover:bg-slate-100 text-left'
+            className='h-8 bg-white px-4 text-left text-sm capitalize text-black outline-none hover:bg-slate-100'
             defaultValue=''
           >
             <option value='' disabled>
@@ -31,26 +62,26 @@ export default function SortProductList() {
             <span>/2</span>
           </div>
           <div className='ml-2'>
-            <button className='shadow px-3 h-8 rounded-tl-sm rounded-bl-sm bg-white/60 hover:bg-slate-100 cursor-not-allowed'>
+            <button className='h-8 cursor-not-allowed rounded-bl-sm rounded-tl-sm bg-white/60 px-3 shadow hover:bg-slate-100'>
               <svg
                 xmlns='http://www.w3.org/2000/svg'
                 fill='none'
                 viewBox='0 0 24 24'
                 strokeWidth={1.5}
                 stroke='currentColor'
-                className='w-3 h-3'
+                className='h-3 w-3'
               >
                 <path strokeLinecap='round' strokeLinejoin='round' d='M15.75 19.5L8.25 12l7.5-7.5' />
               </svg>
             </button>
-            <button className='shadow px-3 h-8 rounded-tr-sm rounded-br-sm bg-white hover:bg-slate-100'>
+            <button className='h-8 rounded-br-sm rounded-tr-sm bg-white px-3 shadow hover:bg-slate-100'>
               <svg
                 xmlns='http://www.w3.org/2000/svg'
                 fill='none'
                 viewBox='0 0 24 24'
                 strokeWidth={1.5}
                 stroke='currentColor'
-                className='w-3 h-3'
+                className='h-3 w-3'
               >
                 <path strokeLinecap='round' strokeLinejoin='round' d='M8.25 4.5l7.5 7.5-7.5 7.5' />
               </svg>
