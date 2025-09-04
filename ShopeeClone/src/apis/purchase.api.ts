0a3e746d085d41c76ee9fefdb2500352dd86969b
Tuandev18 <phamtuan180704@gmail.com>
2025-09-04 15:53:41 +0700

    Analysis and declaration of API purchases

---
+++ ShopeeClone/src/apis/purchase.api.ts
@@ -0,0 +1,19 @@
+import type { Purchase, PurchaseListStatus } from "src/types/purchase.type";
+import type { SuccessResponseApi } from "src/types/utils.type";
+import http from "src/utils/htpp";
+
+
+const URL = 'purchases'
+
+const purchaseApi = {
+  addToCart(body : {product_id : string; buy_count : number}){
+    return http.post<SuccessResponseApi<Purchase>>(`${URL}/add-to-cart`,body)
+  },
+  getPurchases(params : {status : PurchaseListStatus}){
+    return http.get<SuccessResponseApi<Purchase[]>>(`${URL}`,{
+      params
+    })
+  }
+}
+
+export default purchaseApi
