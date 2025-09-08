0497c230742ef63b49a3e89f7331a305b4adc9be
Tuandev18 <phamtuan180704@gmail.com>
2025-09-08 16:20:20 +0700

    Code UI Pagr Profile

--- ShopeeClone/src/pages/User/pages/Profile/Profile.tsx
+++ ShopeeClone/src/pages/User/pages/Profile/Profile.tsx
@@ -1,6 +1,89 @@
+import Input from 'src/components/Input'
 
 export default function Profile() {
   return (
-    <div>Profile</div>
+  
+    <div className='rounded-sm bg-white px-4 pb-10 shadow md:px-7 md:pb-20'>
+      <div className='border-b border-b-gray-200 py-6'>
+        <h1 className='text-lg font-medium capitalize text-gray-900'>Hồ Sơ Của Tôi</h1>
+        <div className='mt-1 text-sm text-gray-700'>Quản lý thông tin hồ sơ để bảo mật tài khoản</div>
+      </div>
+
+      <div className='mt-8 flex flex-col-reverse md:flex-row md:items-start'>
+        {/* Form thông tin */}
+        <form className='mt-6 flex-grow md:mt-0 md:pr-12'>
+          {/* Email */}
+          <div className='flex flex-col sm:flex-row sm:items-center'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Email</div>
+            <div className='sm:w-[80%] sm:pl-5'>
+              <div className='pt-3 text-gray-700'>du***********@gmail.com</div>
+            </div>
+          </div>
+
+          {/* Tên */}
+          <div className='mt-6 flex flex-col sm:flex-row sm:items-center'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Tên</div>
+            <div className='sm:w-[80%] sm:pl-5'>
+              <Input classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm' />
+            </div>
+          </div>
+
+          {/* Số điện thoại */}
+          <div className='mt-4 flex flex-col sm:flex-row sm:items-center'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Số điện thoại</div>
+            <div className='sm:w-[80%] sm:pl-5'>
+              <Input classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm' />
+            </div>
+          </div>
+
+          {/* Địa chỉ */}
+          <div className='mt-4 flex flex-col sm:flex-row sm:items-center'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Địa chỉ</div>
+            <div className='sm:w-[80%] sm:pl-5'>
+              <Input classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm' />
+            </div>
+          </div>
+
+          {/* Ngày sinh */}
+          <div className='mt-4 flex flex-col sm:flex-row sm:items-center'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Ngày sinh</div>
+            <div className='sm:w-[80%] sm:pl-5'>
+              <div className='flex justify-between gap-2'>
+                <select className='h-10 w-[32%] rounded-sm border border-black/10 px-3'>
+                  <option disabled>Ngày</option>
+                </select>
+                <select className='h-10 w-[32%] rounded-sm border border-black/10 px-3'>
+                  <option disabled>Tháng</option>
+                </select>
+                <select className='h-10 w-[32%] rounded-sm border border-black/10 px-3'>
+                  <option disabled>Năm</option>
+                </select>
+              </div>
+            </div>
+          </div>
+        </form>
+
+        {/* Avatar */}
+        <div className='mb-6 flex justify-center md:mb-0 md:w-72 md:border-l md:border-l-gray-200'>
+          <div className='flex flex-col items-center'>
+            <div className='my-5 h-24 w-24'>
+              <img
+                src='https://cf.shopee.vn/file/d04ea22afab6e6d250a370d7ccc2e675_tn'
+                alt='avatar'
+                className='h-full w-full rounded-full object-cover'
+              />
+            </div>
+            <input className='hidden' type='file' accept='.jpg,.jpeg,.png' />
+            <button className='flex h-10 items-center justify-center rounded-sm border bg-white px-6 text-sm text-gray-600 shadow-sm'>
+              Chọn ảnh
+            </button>
+            <div className='mt-3 text-gray-400'>
+              <div>Dung lượng file tối đa 1 MB</div>
+            </div>
+            <div>Định dạng: .JPEG, .PNG</div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
