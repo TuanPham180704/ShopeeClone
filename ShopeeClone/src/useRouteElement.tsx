21d38ea8be6d8cb877e29e647a0fdbeadeedf285
Tuandev18 <phamtuan180704@gmail.com>
2025-09-17 17:39:24 +0700

    Remove import component after Lazyload

--- ShopeeClone/src/useRouteElement.tsx
+++ ShopeeClone/src/useRouteElement.tsx
@@ -1,23 +1,11 @@
 import { useContext, lazy, Suspense } from 'react'
 import { Navigate, Outlet, useRoutes } from 'react-router-dom'
-// import Cart from 'src/components/Cart'
-
 import path from 'src/constants/path'
 import { AppContext } from 'src/contexts/app.contexts'
 import CartLayout from 'src/layouts/CartLayout'
 import UserLayout from 'src/pages/User/layouts/UserLayout'
 import MainLayout from 'src/layouts/MainLayout'
 import RegisterLayout from 'src/layouts/RegisterLayout'
-// import Login from 'src/pages/Login'
-// import ProducDetail from 'src/pages/ProducDetail'
-// import ProductList from 'src/pages/ProductList'
-// import Profile from 'src/pages/User/pages/Profile'
-// import Register from 'src/pages/Register'
-
-// import ChangePassword from 'src/pages/User/pages/ChangePassword'
-// import HistoryPurchase from 'src/pages/User/pages/HistoryPurchase'
-// import NotFound from 'src/pages/NotFound'
-
 const Login = lazy(() => import('src/pages/Login'))
 const ProducDetail = lazy(() => import('src/pages/ProducDetail'))
 const ProductList = lazy(() => import('src/pages/ProductList'))
